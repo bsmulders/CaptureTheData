34d2d163e68ab19a8c913f8ae2ef1e1373fa7218
Bobbie Smulders <mail@bsmulders.com>
2011-12-30 06:27:08 +0100

    Added Wii logging support.

--- ctdlogger/src/wii.c
+++ ctdlogger/src/wii.c
@@ -13,16 +13,63 @@
 
 #include "wii.h"
 #include "common.h"
-#include "wiiuse.h"
 
-#include <stddef.h>
 #include <sqlite3.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <wiiuse.h>
 
 int log_wii(char * device, char * database, int tripid) {
+	int retval;
+	sqlite3 *handle;
+	wiimote** wiimotes;
+	int found, connected;
+
+	// Open database connection
+	retval = sqlite3_open(database, &handle);
+	if (retval) {
+		printf("Wii Reading: Database connection failed: %d\n", retval);
+		return -1;
+	}
+
+	// Connect to wiimote
+	wiimotes = wiiuse_init(1);
+
+	found = wiiuse_find(wiimotes, 1, 5);
+	if (!found) {
+		printf("Wii Reading: No wiimote found\n");
+		return -1;
+	}
+
+	connected = wiiuse_connect(wiimotes, 1);
+	if (!connected) {
+		printf("Wii Reading: Failed to connect to wiimote\n");
+		return -1;
+	}
+
+	wiimote* wiimote = wiimotes[0];
+	wiiuse_motion_sensing(wiimote, 1);
+
+	// Gather wii data
+	while (1) {
+		if (wiiuse_poll(wiimotes, 1)) {
+			if (wiimote->event == WIIUSE_EVENT && WIIUSE_USING_ACC(wiimotes[0])) {
+				//Insert serial data in database
+				char query[250];
+				double timestamp = current_time_with_ms();
+				sprintf(
+						query,
+						"INSERT INTO WiiData ('Trip_ID', 'TimeStamp', 'GforceX', 'GForceY', 'GForceZ', 'Roll', 'Pitch', 'Yaw' ) VALUES ( %d, %f, %f, %f, %f, %f, %f, %f )", tripid, timestamp, wiimote->gforce.x, wiimote->gforce.y, wiimote->gforce.z, wiimote->orient.roll, wiimote->orient.pitch, wiimote->orient.roll);
+				do {
+					retval = sqlite3_exec(handle, query, 0, 0, 0);
+				} while (retval == SQLITE_BUSY);
+			}
+		}
+	}
+
+	// Destroy the evidence!
+	sqlite3_close(handle);
+	wiiuse_cleanup(wiimotes, 1);
+
 	return 0;
 }
 
